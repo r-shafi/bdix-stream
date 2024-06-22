2377f733166bc5b9651cd28b062eac4c865e3aee
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 17:47:29 +0600

    create sports and entertainment

---
+++ app/(routes)/sports/page.tsx
@@ -0,0 +1,9 @@
+import Table from '@/app/components/Table';
+import { StreamLink } from '@/types/interface';
+import { getStreams } from '@/utilities/actions';
+
+export default async function Home() {
+  const streams: StreamLink[] = await getStreams('sports');
+
+  return <div>{<Table data={streams}></Table>}</div>;
+}
