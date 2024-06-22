99c1a0181057f2a2a0a22d26871bc0bf47579cbe
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 14:22:14 +0600

    update fields

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -4,6 +4,7 @@ interface InputProps {
   autoComplete?: string;
   required?: boolean;
   placeholder?: string;
+  name?: string;
 }
 
 const Input = ({
@@ -12,6 +13,7 @@ const Input = ({
   autoComplete,
   required = false,
   placeholder,
+  name = label,
 }: InputProps) => {
   return (
     <label className="text-sm font-medium text-gray-900 flex flex-col gap-2">
@@ -22,6 +24,7 @@ const Input = ({
         required={required}
         className="outline-none bg-gray-50 border border-gray-300 focus:border-blue-400 text-gray-700 rounded-lg w-full p-2.5"
         placeholder={placeholder}
+        name={name}
       />
     </label>
   );
