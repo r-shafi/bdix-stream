ea2964cbc19acfdaf2f0f859358b425b8e4eaf8f
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 12:25:33 +0600

    create button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,13 @@
+interface ButtonProps {
+  title: string;
+}
+
+const Button = ({ title }: ButtonProps) => {
+  return (
+    <button className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">
+      {title}
+    </button>
+  );
+};
+
+export default Button;
