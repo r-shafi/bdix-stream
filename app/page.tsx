a69e65cd7f3206d592a2fc2f749ca198521e84fb
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 17:19:21 +0600

    display links on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import { getStreams } from '@/utilities/actions';
+import Table from './components/Table';
+
 export default async function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  const streams: StreamLink[] = await getStreams();
+
+  const columns = [
+    'Title',
+    'Description',
+    'URL',
+    'Type',
+    'Votes',
+    'User',
+    'Created At',
+  ];
+  const rows = streams.map((stream) => [
+    stream.title,
+    stream.description,
+    stream.url,
+    stream.type,
+    `${stream.upvotes}/${stream.downvotes}`,
+    stream.user.username,
+    stream.createdAt,
+  ]);
+
+  return <div>{<Table columns={columns} rows={rows}></Table>}</div>;
+}
+
+export interface StreamLink {
+  _id: string;
+  user: {
+    _id: string;
+    username: string;
+  };
+  title: string;
+  description: string;
+  url: string;
+  upvotes: number;
+  downvotes: number;
+  type: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
 }
