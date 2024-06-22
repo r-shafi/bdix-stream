03f1462ed7d25479308dd0203c500689522e00a4
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 17:47:37 +0600

    reduce code

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,9 @@
+import { StreamLink } from '@/types/interface';
 import { getStreams } from '@/utilities/actions';
 import Table from './components/Table';
 
 export default async function Home() {
   const streams: StreamLink[] = await getStreams();
 
-  const columns = [
-    'Title',
-    'Description',
-    'URL',
-    'Type',
-    'Votes',
-    'User',
-    'Created At',
-  ];
-  const rows = streams.map((stream) => [
-    stream.title,
-    stream.description,
-    stream.url,
-    stream.type,
-    `${stream.upvotes}/${stream.downvotes}`,
-    stream.user.username,
-    stream.createdAt,
-  ]);
-
-  return <div>{<Table columns={columns} rows={rows}></Table>}</div>;
-}
-
-export interface StreamLink {
-  _id: string;
-  user: {
-    _id: string;
-    username: string;
-  };
-  title: string;
-  description: string;
-  url: string;
-  upvotes: number;
-  downvotes: number;
-  type: string;
-  createdAt: Date;
-  updatedAt: Date;
-  __v: number;
+  return <div>{<Table data={streams}></Table>}</div>;
 }
