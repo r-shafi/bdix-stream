1262b51955d64057056633395465651f10e29915
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 19:01:24 +0600

    create vote button

---
+++ app/components/VoteButton.tsx
@@ -0,0 +1,43 @@
+interface VoteButtonProps {
+  type: 'upvote' | 'downvote';
+  count: number;
+}
+
+const VoteButton = ({ type, count }: VoteButtonProps) => {
+  return (
+    <button className="flex items-center gap-2 bg-transparent hover:bg-gray-100 transition-all px-2 py-1 rounded-md">
+      {type === 'upvote' ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#2c3e50"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#2c3e50"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3" />
+        </svg>
+      )}
+      <span>{count}</span>
+    </button>
+  );
+};
+
+export default VoteButton;
