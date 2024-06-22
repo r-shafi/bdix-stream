354844a868e27fb06d226242646aff834280b01c
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 19:50:49 +0600

    update link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,12 +12,12 @@ const Header = () => {
   return (
     <nav className="bg-white border-gray-200 backdrop-blur-md">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
+        <Link
           className="self-center text-2xl text-gray-600 font-semibold whitespace-nowrap"
           href="/"
         >
           m3u8
-        </a>
+        </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link href="/new">
             <Button title="Add New Link"></Button>
@@ -25,7 +25,12 @@ const Header = () => {
         </div>
         <div className="hidden md:flex items-center gap-4">
           {LINKS.map((link) => (
-            <Link key={link.title} href={link.href}>
+            // <AnchorLink title={link.title} href={link.href} key={link.href} />
+            <Link
+              href={link.href}
+              key={link.href}
+              className="lowercase text-gray-600 hover:text-gray-800 hover:underline"
+            >
               {link.title}
             </Link>
           ))}
