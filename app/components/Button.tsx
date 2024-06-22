3d1176200c8d7ba29844127da7f218563ea73b26
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 14:22:26 +0600

    update button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,14 @@
 interface ButtonProps {
   title: string;
+  type?: 'button' | 'submit';
 }
 
-const Button = ({ title }: ButtonProps) => {
+const Button = ({ title, type = 'button' }: ButtonProps) => {
   return (
-    <button className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">
+    <button
+      className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
+      type={type}
+    >
       {title}
     </button>
   );
