c8e86fb8c31c3a311f8aaefe77e16acc7022509c
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 14:27:40 +0600

    create login page

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -1,27 +1,40 @@
 import Button from '@/app/components/Button';
+import Input from '@/app/components/Input';
+import { login } from '@/utilities/actions';
+import { redirect } from 'next/navigation';
 
-const Page = () => {
+const FIELDS = [
+  {
+    label: 'Username',
+    type: 'text',
+    autoComplete: 'given-name',
+  },
+  {
+    label: 'Password',
+    type: 'password',
+    autoComplete: 'current-password',
+  },
+];
+
+const Page = async () => {
   return (
     <div className="flex justify-center items-center min-h-[85vh]">
-      <form className="flex flex-col gap-4">
-        <label className="text-sm font-medium text-gray-900 flex flex-col gap-2">
-          Username
-          <input
-            type="text"
-            autoComplete="given-name"
-            className="outline-none bg-gray-50 border border-gray-300 focus:border-blue-400 text-gray-700 rounded-lg w-full p-2.5"
-            required
-          />
-        </label>
-        <label className="text-sm font-medium text-gray-900 flex flex-col gap-2">
-          Password
-          <input
-            type="password"
-            autoComplete="current-password"
-            className="outline-none bg-gray-50 border border-gray-300 focus:border-blue-400 text-gray-700 rounded-lg w-full p-2.5"
-            required
-          />
-        </label>
+      <form
+        className="flex flex-col gap-4"
+        action={async (formData) => {
+          'use server';
+          await login(formData);
+          redirect('/new');
+        }}
+      >
+        {FIELDS.map(({ label, type, autoComplete }, i) => (
+          <Input
+            key={i}
+            label={label}
+            type={type}
+            autoComplete={autoComplete}
+          ></Input>
+        ))}
 
         <Button title="Login / Register" type="submit"></Button>
       </form>
