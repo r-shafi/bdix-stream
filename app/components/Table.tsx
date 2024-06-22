d522f0aada7993ee8dc3292645f1cb836533fdf3
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-22 18:27:38 +0600

    remove dark styles

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -31,8 +31,8 @@ const Table = ({ data }: TableProps) => {
 
   return (
     <div className="relative overflow-x-auto">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50  ">
           <tr>
             {columns.map((column) => (
               <th scope="col" className="px-6 py-3" key={column}>
