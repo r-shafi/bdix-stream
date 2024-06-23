344bc92b48169d8447e65bd8b63b33432dd9e34c
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-23 20:57:52 +0600

    create vote method

--- utilities/api/stream.ts
+++ utilities/api/stream.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { StreamModel } from '@/schemas/stream.schema';
+import { VoteModel } from '@/schemas/vote.schema';
+import { revalidatePath } from 'next/cache';
 import { response } from '../functions/functions';
 import { getSession } from './actions';
 
@@ -45,6 +47,55 @@ export async function createStream(formData: FormData) {
     await StreamModel.create(stream);
 
     return response(undefined, false, 'Stream added');
+  } catch (error) {
+    console.log(error);
+    return response(error, true);
+  }
+}
+
+export async function voteStream(id: string, vote: 'upvote' | 'downvote') {
+  const session = await getSession();
+
+  if (!session) {
+    return response(undefined, true, 'You must be logged in to vote');
+  }
+
+  try {
+    const stream = await StreamModel.findById(id, 'upvotes downvotes');
+
+    if (!stream) {
+      return response(undefined, true, 'Stream not found');
+    }
+
+    const existingVote = await VoteModel.findOne(
+      {
+        user: session.id,
+        stream: id,
+      },
+      'type'
+    );
+
+    if (existingVote && existingVote.type === vote) {
+      existingVote.type = 'removed';
+      await existingVote.save();
+
+      stream[`${vote}s`] -= 1;
+      await stream.save();
+
+      return response(undefined, false, 'Vote removed');
+    }
+
+    const newVote = await VoteModel.create({
+      user: session.id,
+      stream: id,
+      type: vote,
+    });
+
+    vote === 'upvote' ? (stream.upvotes += 1) : (stream.downvotes += 1);
+    await stream.save();
+
+    revalidatePath('/');
+    return response(undefined, false, 'Vote added');
   } catch (error) {
     console.log(error);
     return response(error, true);
