0e0e2ad62937be3625a2ecc703f618c10616e5b6
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-24 08:53:46 +0600

    create stream play button

---
+++ app/components/PlayButton.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+const PlayButton = ({ src }: { src: string }) => {
+  const base64 = Buffer.from(src).toString('base64');
+  console.log(base64);
+
+  return (
+    <Link
+      href={{
+        pathname: '/play',
+        query: { src: base64 },
+      }}
+      className="py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 flex items-center gap-2"
+    >
+      <svg
+        className="h-4 w-4"
+        data-slot="icon"
+        fill="none"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+        ></path>
+      </svg>
+      Play
+    </Link>
+  );
+};
+
+export default PlayButton;
