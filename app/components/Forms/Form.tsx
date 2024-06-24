a3651ad8704b0077af52e83b6c3daf6ca3e4c249
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-24 16:11:01 +0600

    refactor code

--- app/components/Forms/Form.tsx
+++ app/components/Forms/Form.tsx
@@ -1,39 +1,16 @@
 import { FormProps } from '@/types/interface';
-import FormButton from './Buttons/FormButton';
+import FormButton from '../Buttons/FormButton';
 import Input from './Input';
+import Select from './Select';
 
 const Form = ({ action, fields, buttonTitle }: FormProps) => {
   return (
     <form className="flex flex-col gap-4" action={action}>
-      {fields.map(({ label, type, required, placeholder, options }, i) =>
-        type === 'select' ? (
-          <label
-            key={i}
-            className="flex flex-col gap-2 text-sm font-medium text-gray-900"
-          >
-            {label}
-            <select
-              className="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              name={label.toLowerCase()}
-            >
-              <option value="" disabled selected>
-                {placeholder || 'Select an option'}
-              </option>
-              {options?.map(({ value, label }, i) => (
-                <option key={i} value={value}>
-                  {label}
-                </option>
-              ))}
-            </select>
-          </label>
+      {fields.map((field, i) =>
+        field.type === 'select' ? (
+          <Select key={i} {...field} />
         ) : (
-          <Input
-            key={i}
-            label={label}
-            type={type}
-            required={required}
-            placeholder={placeholder}
-          />
+          <Input key={i} {...field} />
         )
       )}
 
