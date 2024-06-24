79385e921a823647dc4ab77475c9aea367d85c93
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-24 16:11:21 +0600

    refactor and conform with existing code

--- app/components/Forms/Input.tsx
+++ app/components/Forms/Input.tsx
@@ -1,30 +1,20 @@
-interface InputProps {
-  label: string;
-  type: string;
-  autoComplete?: string;
-  required?: boolean;
-  placeholder?: string;
-  name?: string;
-}
+import { Field } from '@/types/interface';
 
 const Input = ({
   label,
   type = 'text',
-  autoComplete,
   required = false,
   placeholder,
-  name = label,
-}: InputProps) => {
+}: Field) => {
   return (
     <label className="text-sm font-medium text-gray-900 flex flex-col gap-2">
       {label}
       <input
         type={type}
-        autoComplete={autoComplete}
         required={required}
         className="outline-none bg-gray-50 border border-gray-300 focus:border-blue-400 text-gray-700 rounded-lg w-full p-2.5"
         placeholder={placeholder}
-        name={name}
+        name={label.toLowerCase()}
       />
     </label>
   );
