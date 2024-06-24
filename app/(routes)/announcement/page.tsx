9d4ea88de670a01afeb15a202318d34b4d431f26
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-24 21:37:19 +0600

    announcement page content

--- app/(routes)/announcement/page.tsx
+++ app/(routes)/announcement/page.tsx
@@ -1,5 +1,52 @@
 const Page = () => {
-  return <h1>Page Under Construction</h1>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="font-bold text-2xl md:text-4xl mb-4">
+        Welcome to Our Live Stream Sharing Community!
+      </h1>
+
+      <p>
+        I am excited to invite you to join my brand-new platform, a place where
+        we all can share any live stream content we’re passionate about. Whether
+        you’re into gaming, live events, educational sessions, or any other type
+        of content, this is your space to connect with others and share your
+        streams.
+      </p>
+
+      <h2 className="font-medium text-xl md:text-2xl mt-4">
+        Our Community Values:
+      </h2>
+      <ul className="flex flex-col gap-4">
+        <li>
+          <b>Freedom to Share</b>: This platform is built for you. Share any
+          live stream content you want and let your creativity shine. Together,
+          we can build a diverse and vibrant community.
+        </li>
+        <li>
+          <b>Responsibility and Respect</b>: While this platform is for
+          everyone, we all share the responsibility of maintaining a respectful
+          and lawful environment. If any content violates the rights of others,
+          report it, and I&apos;ll try to address it as soon as possible.
+        </li>
+        <li>
+          <b>Your Voice Matters</b>: This is your community. If you come across
+          content that doesn’t align with our values or violates someone’s
+          rights, report it. Your feedback helps keep our platform safe and
+          enjoyable for everyone.
+        </li>
+      </ul>
+
+      <p>
+        Thank you for being a part of this incredible journey. I can’t wait to
+        see the amazing live streams you’ll share. Let’s create, connect, and
+        celebrate together!
+      </p>
+
+      <p className="mt-4">
+        Happy Streaming! <br /> Shafi Rayhan
+      </p>
+    </div>
+  );
 };
 
 export default Page;
