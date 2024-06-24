aec7d7cecbeb8b001c09ec24df437a8eeb0e776e
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-24 08:27:37 +0600

    update svg error

--- app/components/Toast.tsx
+++ app/components/Toast.tsx
@@ -65,8 +65,8 @@ const Toast = ({ message, type }: ToastProps) => {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             strokeWidth="2"
             d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
           />
