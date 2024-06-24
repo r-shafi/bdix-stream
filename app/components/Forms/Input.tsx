7fb785aa8bcf76352ea1b6610640ac0e000d11e4
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-24 16:43:11 +0600

    update props

--- app/components/Forms/Input.tsx
+++ app/components/Forms/Input.tsx
@@ -5,6 +5,8 @@ const Input = ({
   type = 'text',
   required = false,
   placeholder,
+  name,
+  autocomplete,
 }: Field) => {
   return (
     <label className="text-sm font-medium text-gray-900 flex flex-col gap-2">
@@ -14,7 +16,8 @@ const Input = ({
         required={required}
         className="outline-none bg-gray-50 border border-gray-300 focus:border-blue-400 text-gray-700 rounded-lg w-full p-2.5"
         placeholder={placeholder}
-        name={label.toLowerCase()}
+        name={name}
+        autoComplete={autocomplete}
       />
     </label>
   );
--- app/components/Forms/Select.tsx
+++ app/components/Forms/Select.tsx
@@ -1,15 +1,15 @@
 import { Field } from '@/types/interface';
 
-const Select = ({ label, placeholder, options }: Field) => {
+const Select = ({ label, options, name }: Field) => {
   return (
     <label className="flex flex-col gap-2 text-sm font-medium text-gray-900">
       {label}
       <select
         className="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        name={label.toLowerCase()}
+        name={name}
       >
         <option value="" disabled selected>
-          {placeholder || 'Select an option'}
+          Select {label}
         </option>
         {options?.map(({ value, label }, i) => (
           <option key={i} value={value}>
