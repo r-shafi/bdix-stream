bea420e55ae4dfb0f27bf716fbac24b5841b2b1e
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-28 23:58:21 +0600

    create global forms

---
+++ utilities/forms/forms.ts
@@ -0,0 +1,99 @@
+const forms = {
+  loginForm: [
+    {
+      label: 'Username',
+      type: 'text',
+      required: true,
+      placeholder: 'Username',
+      name: 'username',
+      autocomplete: 'given-name',
+      validation: {
+        minLen: 5,
+        maxLen: 32,
+        pattern: /^[a-zA-Z0-9_]+$/,
+      },
+    },
+    {
+      label: 'Password',
+      type: 'password',
+      required: true,
+      placeholder: '****',
+      name: 'password',
+      autocomplete: 'current-password',
+      validation: {
+        minLen: 6,
+        maxLen: 32,
+      },
+    },
+  ],
+  registerForm: [
+    {
+      label: 'Username',
+      type: 'text',
+      required: true,
+      placeholder: 'Username',
+      name: 'username',
+      autocomplete: 'given-name',
+      validation: {
+        minLen: 5,
+        maxLen: 32,
+        pattern: /^[a-zA-Z0-9_]+$/,
+      },
+    },
+    {
+      label: 'Email',
+      type: 'email',
+      required: false,
+      placeholder: 'Email (optional)',
+      name: 'email',
+      autocomplete: 'email',
+    },
+    {
+      label: 'Password',
+      type: 'password',
+      required: true,
+      placeholder: '****',
+      name: 'password',
+      autocomplete: 'current-password',
+      validation: {
+        minLen: 6,
+        maxLen: 32,
+      },
+    },
+  ],
+  createStreamForm: [
+    {
+      label: 'Title',
+      type: 'text',
+      required: true,
+      placeholder: 'T-Sports - Ban vs Aus',
+      name: 'title',
+    },
+    {
+      label: 'Description',
+      type: 'text',
+      required: false,
+      name: 'description',
+    },
+    {
+      label: 'URL',
+      type: 'url',
+      required: true,
+      name: 'url',
+    },
+    {
+      label: 'Stream Type',
+      type: 'select',
+      required: true,
+      options: [
+        { value: 'cricket', label: 'Cricket' },
+        { value: 'football', label: 'Football' },
+        { value: 'entertainment', label: 'Entertainment' },
+        { value: 'news', label: 'News' },
+      ],
+      name: 'type',
+    },
+  ],
+} as any;
+
+export default forms;
