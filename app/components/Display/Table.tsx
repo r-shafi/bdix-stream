0b3839ce6b3efcc592e8c29f1eed994b437dd05e
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-29 15:56:20 +0600

    refactor table ui

--- app/components/Display/Table.tsx
+++ app/components/Display/Table.tsx
@@ -1,4 +1,5 @@
 import { StreamLink } from '@/types/interface';
+import { FC } from 'react';
 import ClipboardButton from '../Buttons/ClipboardButton';
 import PlayButton from '../Buttons/PlayButton';
 import VoteButton from '../Buttons/VoteButton';
@@ -8,26 +9,28 @@ interface TableProps {
   data: StreamLink[];
 }
 
-const Table = ({ data }: TableProps) => {
-  const columns = [
-    'Title',
-    'Description',
-    'Stream',
-    'Type',
-    'Uploader',
-    'Created At',
-    'Votes',
-  ];
+interface TableRow {
+  title: string;
+  description: string;
+  url: string;
+  type: string;
+  username: string;
+  role: 'user' | 'admin' | 'moderator';
+  createdAt: Date;
+  upvotes: number;
+  downvotes: number;
+  _id: string;
+}
 
-  const rows: any[][] = data.map((stream) => [
-    stream.title,
-    stream.description,
-    stream.url,
-    stream.type,
-    stream.user.username,
-    stream.createdAt,
-    [stream.upvotes, stream.downvotes, stream._id],
-  ]);
+const Table: FC<TableProps> = ({ data }) => {
+  const formatDate = (dateString: Date) => {
+    return Intl.DateTimeFormat('en-US', {
+      month: 'short',
+      day: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+    }).format(new Date(dateString));
+  };
 
   if (data.length === 0) {
     return <div className="text-center">No data found</div>;
@@ -38,7 +41,15 @@ const Table = ({ data }: TableProps) => {
       <table className="w-full text-left text-gray-600 rounded-md overflow-hidden">
         <thead className="text-xs text-gray-800 uppercase bg-gray-50">
           <tr>
-            {columns.map((column) => (
+            {[
+              'Title',
+              'Description',
+              'Stream',
+              'Type',
+              'Uploader',
+              'Created At',
+              'Votes',
+            ].map((column) => (
               <th scope="col" className="px-6 py-3" key={column}>
                 {column}
               </th>
@@ -47,52 +58,54 @@ const Table = ({ data }: TableProps) => {
         </thead>
 
         <tbody>
-          {rows.map((row: any, i) => (
-            <tr className="bg-white border-b" key={i}>
-              <td className="px-6 py-4">{row[0]}</td>
-              <td className="px-6 py-4">{row[1] ? row[1] : '-'}</td>
-              <td className="px-6 py-4 flex items-center gap-4">
-                <PlayButton
-                  stream={{
-                    title: row[0],
-                    description: row[1],
-                    url: row[2],
-                    type: row[3],
-                    user: { _id: '', username: row[4] },
-                    createdAt: row[5],
-                    upvotes: row[6][0],
-                    downvotes: row[6][1],
-                    _id: row[6][2],
-                  }}
-                />
-                <ClipboardButton src={row[2]} />
-              </td>
-              <td className="px-6 py-4" title={row[3]}>
-                <StreamIcon type={row[3]} />
-              </td>
-              <td className="px-6 py-4">{row[4]}</td>
-              <td className="px-6 py-4">
-                {Intl.DateTimeFormat('en-US', {
-                  month: 'short',
-                  day: '2-digit',
-                  hour: 'numeric',
-                  minute: 'numeric',
-                }).format(new Date(row[5]))}
-              </td>
-              <td className="px-6 py-4 flex items-center gap-1">
-                <VoteButton
-                  id={JSON.stringify(row[6][2])}
-                  type="upvote"
-                  count={row[6][0]}
-                ></VoteButton>
-                <VoteButton
-                  id={JSON.stringify(row[6][2])}
-                  type="downvote"
-                  count={row[6][1]}
-                ></VoteButton>
-              </td>
-            </tr>
-          ))}
+          {data.map((stream, index) => {
+            const row: TableRow = {
+              title: stream.title,
+              description: stream.description ?? '-',
+              url: stream.url,
+              type: stream.type,
+              username: stream.user.username,
+              role: stream.user.role,
+              createdAt: stream.createdAt,
+              upvotes: stream.upvotes,
+              downvotes: stream.downvotes,
+              _id: stream._id.toString(),
+            };
+
+            return (
+              <tr className="bg-white border-b" key={index}>
+                <td className="px-6 py-4">{row.title}</td>
+                <td className="px-6 py-4">{row.description}</td>
+                <td className="px-6 py-4 flex items-center gap-4">
+                  <PlayButton id={row._id} />
+                  <ClipboardButton src={row.url} />
+                </td>
+                <td className="px-6 py-4" title={row.type}>
+                  <StreamIcon type={row.type} />
+                </td>
+                <td
+                  className={`px-6 py-4 ${
+                    row.role === 'admin'
+                      ? 'text-red-500'
+                      : row.role === 'moderator'
+                      ? 'text-blue-500'
+                      : ''
+                  }`}
+                >
+                  {row.username}
+                </td>
+                <td className="px-6 py-4">{formatDate(row.createdAt)}</td>
+                <td className="px-6 py-4 flex items-center gap-1">
+                  <VoteButton id={row._id} type="upvote" count={row.upvotes} />
+                  <VoteButton
+                    id={row._id}
+                    type="downvote"
+                    count={row.downvotes}
+                  />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
