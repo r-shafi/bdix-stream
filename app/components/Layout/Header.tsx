ced5e5192d0fe1ca5db0e2b90a43ed40de98bdf7
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-06-30 08:41:07 +0600

    show login button

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,19 +1,31 @@
+import { getSession } from '@/utilities/functions/auth';
 import Link from 'next/link';
 import Button from '../Buttons/Button';
 import NavLinks from '../Display/NavLinks';
 
-const Header = () => {
+const Header = async () => {
+  const session = await getSession();
+
   return (
     <nav className="bg-white border-gray-200 backdrop-blur-md border-b">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link className="text-2xl font-semibold whitespace-nowrap" href="/">
-          BDIX Live Stream
+        <Link
+          className="text-xl md:text-2xl font-semibold whitespace-nowrap flex flex-col md:flex-row"
+          href="/"
+        >
+          BDIX <span className="text-sm md:text-2xl">Live Stream</span>
         </Link>
         <div className="flex items-center gap-4">
           <NavLinks />
-          <Link href="/share">
-            <Button title="Add New Link"></Button>
-          </Link>
+          {session ? (
+            <Link href="/share">
+              <Button title="Add Stream"></Button>
+            </Link>
+          ) : (
+            <Link href="/authenticate">
+              <Button title="Login"></Button>
+            </Link>
+          )}
         </div>
       </div>
     </nav>
